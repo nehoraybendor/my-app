e2189a4625b4c8d1d22fe6f3fec0882740dcacbb
nehoray <nehoraybendor@gmail.com>
2022-10-24 14:55:23 +0300

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import First from './components/first';
+import Second from './components/second';
 
 function App() {
   return (
     <div className="App">
+     {/* <Second/> */}
      <First/>
     </div>
   );
---
+++ src/components/second.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class Second extends Component {
+  render() {
+    return (
+      <div className='second'>Second💖🙌😚</div>
+    )
+  }
+}

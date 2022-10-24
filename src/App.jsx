5904651c32f67992b9dd36946a952ae6670dd198
nehoray <nehoraybendor@gmail.com>
2022-10-24 12:51:44 +0300

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import First from './components/first';
 
 function App() {
   return (
     <div className="App">
-      
+     <First/>
     </div>
   );
 }
---
+++ src/components/first.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class First extends Component {
+  render() {
+    return (
+      <div>First</div>
+    )
+  }
+}

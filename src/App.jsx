2ec97a8a7b081e5787a09cddc259cba5be5c83b0
nehoray <nehoraybendor@gmail.com>
2022-12-26 10:41:00 +0200

    xxxx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
+import { useState } from 'react';
 import './App.css';
+import { AppContext } from './context/context';
 import AppRouters from './routers/appRouters';
 
 
 function App() {
+  const [user,setUser] = useState({name:"nehoray",age:21})
   return (
     <div >
+      <AppContext.Provider value = {(
+        setUser,user
+      )}>
       <AppRouters />
+      </AppContext.Provider>
     </div>
   );
 }
